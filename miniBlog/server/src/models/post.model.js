ec47f24b73069fcb3756e7db109f9d3548667309
ashreekar <ashreekar767@gmail.com>
2025-08-30 16:14:02 +0530

    add post model

--- miniBlog/server/src/models/post.model.js
+++ miniBlog/server/src/models/post.model.js
@@ -1,2 +1,26 @@
 import mongoose from "mongoose";
+import aggregatePaginate from "mongoose-aggregate-paginate-v2";
 
+
+const postSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    images:[{
+        type:String
+    }],
+    author:{
+        type:mongoose.Types.ObjectId,
+        ref:"User"
+    },
+    content:{
+        type:String
+    }
+},{
+    timestamps:true
+})
+
+postSchema.plugin(aggregatePaginate);
+
+export const Post=mongoose.model("Post",postSchema);
